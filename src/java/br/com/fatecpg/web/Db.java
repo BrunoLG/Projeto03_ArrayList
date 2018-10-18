77e2c4ffd99520ed2ed6e72ef64465389487efbb
bruno <bruno@BRUNO-PC>
2018-10-18 00:00:54 -0300

    Criação do arquivo Db para as páginas de Veiculo

--- src/java/br/com/fatecpg/web/Db.java
+++ src/java/br/com/fatecpg/web/Db.java
@@ -5,10 +5,29 @@
  */
 package br.com.fatecpg.web;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bruno
  */
 public class Db {
+    private static final ArrayList<Veiculo> veiculos = new ArrayList<>();
     
+    public static ArrayList<Veiculo> getVeiculos(){
+        if (veiculos.isEmpty()){
+            Veiculo c1 = new Veiculo();
+            c1.setDados("MKH-2730", "Nissan", "Versa", "Preto");
+            veiculos.add(c1);
+           
+            Veiculo c2 = new Veiculo();
+            c2.setDados("NAU-1622", "Renault", "Duster", "Vermelho");
+            veiculos.add(c2);
+            
+            Veiculo c3 = new Veiculo();
+            c3.setDados("AVO-0254", "Ford", "Fiesta", "Cinza");
+            veiculos.add(c3);
+        }
+        return veiculos;
+    }
 }
